b7beb3c7ecd662f1856fa00e3f9f4ed6da09f37d
Aleksander Skrede <aleksander.skrede@protonmail.com>
2018-07-17 13:19:57 +0200

    Update question ratings. Two successive questions have a 'match' when one is answered correctly and the other not.

--- classes/capquiz_rating_system.php
+++ classes/capquiz_rating_system.php
@@ -20,14 +20,10 @@ defined('MOODLE_INTERNAL') || die();
 
 abstract class capquiz_rating_system {
 
-    public abstract function user_loss_rating(capquiz_user $user, capquiz_question $question);
+    public abstract function update_user_loss_rating(capquiz_user $user, capquiz_question $question);
 
-    public abstract function user_victory_rating(capquiz_user $user, capquiz_question $question);
+    public abstract function update_user_victory_rating(capquiz_user $user, capquiz_question $question);
 
-    //Must return an array with modified ratings such as: array(first_rating, second_rating)
-    public abstract function question_draw_ratings(capquiz_question $first, capquiz_question $second);
-
-    //Must return an array with modified ratings such as: array(winner_rating, loser_rating)
     public abstract function question_victory_ratings(capquiz_question $winner, capquiz_question $loser);
 
 }
