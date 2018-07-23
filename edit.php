281ee042cc374af105e10d992b9fc7db92cbb965
skrede <aleksander.skrede@gmail.com>
2018-07-23 15:25:04 +0200

    Show error message when trying to delete questions

--- edit.php
+++ edit.php
@@ -22,32 +22,15 @@ require_once($CFG->libdir . '/formslib.php');
 require_once($CFG->dirroot . '/mod/capquiz/lib.php');
 require_once($CFG->dirroot . '/mod/capquiz/utility.php');
 
-function determine_action(capquiz $capquiz, string $action_type) {
-    $capquiz->require_instructor_capability();
-    if ($action_type == capquiz_actions::$redirect) {
-        redirect_to($capquiz);
-    }
-    else if ($action_type == capquiz_actions::$set_question_list) {
-        assign_question_list($capquiz);
-    }
-    else if ($action_type == capquiz_actions::$add_question_to_list) {
-        add_question_to_list($capquiz);
-    }
-    else if ($action_type == capquiz_actions::$remove_question_from_list) {
-        remove_question_from_list($capquiz);
-    }
-    else if ($action_type == capquiz_actions::$publish_question_list) {
-        publish_question_list($capquiz);
-    }
-    else if ($action_type == capquiz_actions::$set_question_rating) {
-        set_question_rating($capquiz);
-    }
-    redirect_to_dashboard($capquiz);
-}
-
-$question_page = optional_param(capquiz_urls::$param_question_page, 0, PARAM_INT);
 if ($capquiz = capquiz::create()) {
-    redirect_to_url(capquiz_urls::view_question_list_url($question_page));
+    if ($delete_selected = optional_param(capquiz_urls::$param_delete_selected, null, PARAM_TEXT)) {
+        echo "Delete is not implemented";
+        return;
+    }
+    else {
+        $question_page = optional_param(capquiz_urls::$param_question_page, 0, PARAM_INT);
+        redirect_to_url(capquiz_urls::view_question_list_url($question_page));
+    }
 }
 
 redirect_to_front_page();
