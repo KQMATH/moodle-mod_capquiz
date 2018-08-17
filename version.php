ce035c07076aa5f7b8f030d857871e43ba39f5bc
skrede <aleksander.skrede@gmail.com>
2018-08-17 14:52:33 +0200

    Fix minor bugs pertaining capabilities and template creation.
    Resolves issue #34 and issue #22

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018081505;
+$plugin->version = 2018081506;
 $plugin->requires = 2016052304;
 $plugin->cron = 0;
 $plugin->component = 'mod_capquiz';
