d7ea09d6633d23bfd4914023009961dbf0a45216
Hans Georg Schaathun <georg+test@schaathun.net>
2018-09-03 23:43:47 +0200

    Fixed a negative percent progress bar.  The colours are not pretty, but the meaning is clear.
    Resolved #39.

--- classes/output/question_attempt_renderer.php
+++ classes/output/question_attempt_renderer.php
@@ -84,16 +84,18 @@ class question_attempt_renderer {
     private function render_progress(capquiz_user $user) {
         $questionlist = $this->capquiz->question_list();
         $percent = $questionlist->next_level_percent($user->rating());
+	$abspercent = $percent ;
         if ( $percent >= 0 ) {
            $progressclass = "capquiz-quiz-progress-fill" ;
         } else {
            $progressclass = "capquiz-quiz-progress-unfill" ;
-           $percent = 0 - $percent ;
+           $abspercent = 0 - $abspercent ;
         }
         return $this->renderer->render_from_template('capquiz/student_progress', [
             'progress' => [
                 'student' => [
                     'percent' => $percent,
+                    'abspercent' => $abspercent,
                     'progressclass' => $progressclass,
                     'stars' => $this->user_star_progress($user, $questionlist)
                 ]
