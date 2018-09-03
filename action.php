f1b1d534195f5d15a25909004eafa86ddb16eb9c
Hans Georg Schaathun @kerckhoffs <georg@schaathun.net>
2018-09-03 21:09:43 +0200

    Attempted fix, copying third argument from the if-clause call to the else-clause call.

--- action.php
+++ action.php
@@ -59,9 +59,12 @@ function remove_capquiz_question(int $question_id, int $question_list_id) {
 function add_question_to_list(capquiz $capquiz) {
     if ($question_id = optional_param(capquiz_urls::$param_question_id, 0, PARAM_INT)) {
         if ($question_list_id = optional_param(capquiz_urls::$param_question_list_id, 0, PARAM_INT)) {
-            create_capquiz_question($question_id, capquiz_question_list::load_question_list($question_list_id), $capquiz->default_question_rating());
+           create_capquiz_question($question_id,
+              capquiz_question_list::load_question_list($question_list_id),
+              $capquiz->default_question_rating());
         } else {
-            create_capquiz_question($question_id, $capquiz->question_list());
+           create_capquiz_question($question_id, $capquiz->question_list(),
+              $capquiz->default_question_rating());
         }
     }
     redirect_to_previous();
