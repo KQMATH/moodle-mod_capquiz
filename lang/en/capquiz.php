db764177513f072ad0098aef3efae7d631218837
Sebastian <sebastian@sgundersen.com>
2019-01-16 17:38:14 +0100

    Started on privacy provider

--- lang/en/capquiz.php
+++ lang/en/capquiz.php
@@ -37,6 +37,8 @@ $string['capquiz:control'] = 'Control a CAPQuiz. (Usually for instructors only)'
 $string['capquiz:editquestions'] = 'Edit questions for an CAPQuiz.';
 $string['capquiz:seeresponses'] = 'View student responses to grade them';
 
+$string['privacy:metadata:core_question'] = 'Question attempts are stored.';
+
 $string['questions_in_list'] = 'Questions in the list';
 $string['add_a_quiz_question'] = 'Add a question to the list';
 $string['add_the_quiz_question'] = 'Add the question to the list';
