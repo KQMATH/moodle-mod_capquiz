4ffa741366a50fb6b9d27008ef1cae624f1625e5
André Storhaug <andr3.storhaug@gmail.com>
2019-07-25 17:34:51 +0200

    Upp the version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019071800;
+$plugin->version = 2019072500;
 $plugin->requires = 2016120500;
 $plugin->cron = 0;
 $plugin->component = 'mod_capquiz';
