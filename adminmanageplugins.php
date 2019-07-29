e698bb5c35e00af7083b2525a37fad57ad332453
André Storhaug <andr3.storhaug@gmail.com>
2019-07-29 19:16:25 +0200

    Remove "defined('MOODLE_INTERNAL') || die();"

--- adminmanageplugins.php
+++ adminmanageplugins.php
@@ -23,8 +23,6 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-defined('MOODLE_INTERNAL') || die();
-
 require_once("../../config.php");
 require_once($CFG->dirroot . '/mod/capquiz/adminlib.php');
 
