d62f234e705226ee90b343bec321306167a86bb8
Hans Georg Schaathun @kerckhoffs <georg@schaathun.net>
2019-10-31 08:44:33 +0100

    Added user id to timereviewed index and updated upgrade script.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019102400;
+$plugin->version = 2019102401;
 $plugin->requires = 2016120500;
 $plugin->cron = 0;
 $plugin->component = 'mod_capquiz';
