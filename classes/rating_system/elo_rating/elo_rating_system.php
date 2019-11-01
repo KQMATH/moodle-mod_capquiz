be01980b8457bd30552b549eb4c4c6f35dd04ee8
Hans Georg Schaathun @kerckhoffs <georg@schaathun.net>
2019-11-01 11:14:57 +0100

    Syntax fix.  Variable not function call

--- classes/rating_system/elo_rating/elo_rating_system.php
+++ classes/rating_system/elo_rating/elo_rating_system.php
@@ -58,7 +58,7 @@ class elo_rating_system extends capquiz_rating_system {
     public function update_user_rating(capquiz_user $user, float $qrating, float $score) {
         $current = $user->rating();
         $factor = $this->studentkfactor;
-        $newrating = $current + $factor * ($score - $this->expected_result($current, $qrating()));
+        $newrating = $current + $factor * ($score - $this->expected_result($current, $qrating));
         $user->set_rating($newrating);
     }
 
