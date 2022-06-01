967e53550b2bdd595d63aafa8c12f52deb7ce941
Majd Saeed Abbas <majdsa@ntnu.no>
2022-06-01 14:51:35 +0200

    checking error

--- classes/bank/add_question_to_list_column.php
+++ classes/bank/add_question_to_list_column.php
@@ -37,7 +37,7 @@ defined('MOODLE_INTERNAL') || die();
  * @copyright   2018 NTNU
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class add_question_to_list_column extends \core_question\local\bank\action_column_base {
+class add_question_to_list_column extends \core_question\bank\action_column_base {
 
     /**
      * Get the internal name for this column. Used as a CSS class name,
